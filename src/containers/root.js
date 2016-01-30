7a3afd8955dd93d5097577a4a14fffee510fdf96
Korakon <i@korakon.com>
2016-01-30 20:25:03 +0100

    remove containers/root

--- src/containers/root.js
+++
@@ -1,9 +0,0 @@
-import { createElement as h } from 'react';
-import { Provider } from 'react-redux';
-import App from 'containers/app';
-
-export default function Root(props) {
-  const { store, handler } = props;
-  return h(Provider, { store },
-           h(App, null, handler));
-}
