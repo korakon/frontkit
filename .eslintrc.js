f35c39e2b01cbfc392eee8f1ffebb63b233f3462
Korakon <i@korakon.com>
2016-01-31 20:25:58 +0100

    move airbnb style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,9 @@
 module.exports = {
-  "rules": {
-    "indent": [
-      2,
-      2
-    ],
-    "quotes": [
-      2,
-      "single"
-    ],
-    "linebreak-style": [
-      2,
-      "unix"
-    ],
-    "semi": [
-      2,
-      "always"
-    ],
-    "no-multiple-empty-lines": [
-      2,
-      {max: 2}
-    ],
-    "key-spacing": [
-      2,
-      {"mode": "strict", "beforeColon": false, "afterColon": true}
-    ],
-    "object-curly-spacing": [2, "always"]
-  },
-  "ecmaFeatures": { "modules": true },
-  "env": {
-    "es6": true,
-    "node": true,
-    "browser": true
-  },
-  "extends": "eslint:recommended"
+    "extends": "airbnb",
+    "rules": {
+      "arrow-body-style": [0, "as-needed"]
+    },
+    "plugins": [
+        "react"
+    ]
 };
