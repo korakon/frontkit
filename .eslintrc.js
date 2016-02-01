fb05ae95994dd24b54450ddc13dfd79d2351d0d8
Korakon <i@korakon.com>
2016-02-01 19:23:47 +0100

    add no-multiple-empty-line rule to .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-      "arrow-body-style": [0, "as-needed"]
+      "arrow-body-style": [ 0, "as-needed" ],
+      "no-multiple-empty-lines": [2, {"max": 1}]
     },
     "plugins": [
         "react"
