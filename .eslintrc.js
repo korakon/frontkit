dea023ff3b305228ff540dd5eafbcfe2ce1afefd
Korakon <i@korakon.com>
2016-02-04 18:19:08 +0100

    Fix switch indentation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
       "arrow-body-style": [ 0, "as-needed" ],
-      "no-multiple-empty-lines": [2, {"max": 1}]
+      "no-multiple-empty-lines": [ 2, { "max": 1 }],
+      "indent": [2, 2, {"SwitchCase": 0}]
     },
     "plugins": [
         "react"
